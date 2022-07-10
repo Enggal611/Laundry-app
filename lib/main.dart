f5a417c9f3f479873bd71f2536eb9cfb47d3f99e
DESKTOP-F01RJJB\ASUS <enggalcahyop@gmail.com>
2022-07-11 00:16:13 +0700

    update plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import '../screen/splash_sc.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+   await Hive.initFlutter();
+  var box = await Hive.openBox('user');
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
